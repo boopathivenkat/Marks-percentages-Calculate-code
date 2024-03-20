f4d4a883ac735de1f0907905d788266c2eac3f50
Boopathi Venkatachalam <159232751+boopathivenkat@users.noreply.github.com>
2024-03-20 10:20:37 +0530

    Add files via upload

---
+++ if else excersize.py
@@ -0,0 +1,11 @@
+a=int(input("Tamil:"))
+b=int(input("English:"))
+c=int(input("Maths:"))
+d=int(input("Science:"))
+e=int(input("Social:"))
+f=(a+b+c+d+e)/5
+print(f)
+if (f<35):
+    print("Your Not elegble for Scholrship")
+else:
+    print("Your Are Good. You Are elegeible")
